32eef8b27c39228335b7f3f76572c2f4b346ad8f
OblojaOana <oblojaoana98@gmail.com>
2021-03-11 12:37:59 +0200

    laborat3 update

---
+++ Laborator3/src/ro/ase/csie/cts/as/laborator3/faza1/TipProdus.java
@@ -0,0 +1,24 @@
+package ro.ase.csie.cts.as.laborator3.faza1;
+
+public enum TipProdus {
+	NOU(0), DISCOUNT(0.1f), STOC_LIMITAT(0.25f), VECHI(0.35f);
+	
+  private final float discount ;
+  
+   private TipProdus(float discount)
+  {
+	  this.discount=discount;
+  }
+   
+   private TipProdus()
+   {
+	   this.discount=0;
+   }
+   
+   public float getDiscount()
+   {
+	   return this.discount;
+   }
+	
+
+}
