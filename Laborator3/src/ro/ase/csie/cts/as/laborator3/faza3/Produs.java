3b4a68d5050cee25623df7af1276ad6b291d71bc
OblojaOana <oblojaoana98@gmail.com>
2021-03-18 13:21:50 +0200

    commit final

--- Laborator3/src/ro/ase/csie/cts/as/laborator3/faza3/Produs.java
+++ Laborator3/src/ro/ase/csie/cts/as/laborator3/faza3/Produs.java
@@ -6,14 +6,41 @@ import ro.ase.csie.cts.as.laborator3.exceptii.ExceptieVechimeClient;
 import ro.ase.csie.cts.as.laborator3.faza1.TipProdus;
 import ro.ase.csie.cts.as.laborator3.faza3.servicii.InterfataMarketing;
 import ro.ase.csie.cts.as.laborator3.faza3.servicii.InterfataValidare;
+import ro.ase.csie.cts.as.laborator3.faza3.servicii.ServiciuValidari;
+import ro.ase.csie.cts.as.laborator3.faza3.servicii.StrategieMarketing2021;
+
+	
 
 public class Produs {
 	
-	InterfataMarketing serviciuMk=null;
-	InterfataValidare serviciuValidare=null;
+	InterfataMarketing serviciuMk = null;
+	InterfataValidare serviciuValidare = null;
 	
-
-
+	
+	public Produs(InterfataMarketing mk, InterfataValidare validare)
+	{
+		if(mk==null)
+		{
+			throw new NullPointerException();
+		}
+		if(validare==null)
+		{
+			throw new NullPointerException();
+		}
+		this.serviciuMk=mk;
+		this.serviciuValidare=validare;
+		
+	}
+	
+	public void setStrategieMarketing(InterfataMarketing strategie)
+	{
+		if(strategie==null)
+		{
+			throw new NullPointerException();
+		}
+		this.serviciuMk=strategie;
+	}
+	
 	public static float getPretCuDiscount(float pretInitial, float discount) {
 		return pretInitial - (discount * pretInitial);
 	}
@@ -24,16 +51,16 @@ public class Produs {
 		
 		serviciuValidare.validarePret(pretInitial);
 		serviciuValidare.validareVechimeClient(vechimeClientInAni);
-		
-	    float discountFidelitate=
-	    		(tipProdus==TipProdus.NOU)? 0: serviciuMk.getDiscountFidelitate(vechimeClientInAni);
-
-		float discount = tipProdus.getDiscount();
-    	float valoareDiscountTipProdus = getPretCuDiscount(pretInitial,discount);
-        float pretFinal = valoareDiscountTipProdus *(1 - discountFidelitate);
+			    
+	    float discountFidelitate = 
+	    		(tipProdus == TipProdus.NOU) ? 0 : serviciuMk.getDiscountFidelitate(vechimeClientInAni);
 	    
-	 
+    	float discount = tipProdus.getDiscount();
+    	float valoareDiscountTipProdus = getPretCuDiscount(pretInitial,discount);
+    	float pretFinal = valoareDiscountTipProdus *(1 - discountFidelitate);
+    	
 	    
 	    return pretFinal;
 	  }
+
 }
---
+++ Laborator3/src/ro/ase/csie/cts/as/laborator3/faza3/TestProdus.java
@@ -0,0 +1,52 @@
+package ro.ase.csie.cts.as.laborator3.faza3;
+
+import java.util.ArrayList;
+
+import ro.ase.csie.cts.as.laborator3.exceptii.ExceptiePretInvalid;
+import ro.ase.csie.cts.as.laborator3.exceptii.ExceptieVechimeClient;
+import ro.ase.csie.cts.as.laborator3.faza3.servicii.InterfataMarketing;
+import ro.ase.csie.cts.as.laborator3.faza3.servicii.InterfataValidare;
+import ro.ase.csie.cts.as.laborator3.faza3.servicii.ServiciuValidari;
+import ro.ase.csie.cts.as.laborator3.faza3.servicii.StrategieMarketing2021;
+
+public class TestProdus {
+	
+	public static ArrayList<Object> servicii=new ArrayList<>();
+	
+	public void startUp()
+	{
+		InterfataMarketing serviciuMk=new StrategieMarketing2021();
+		InterfataValidare serviciuValidare=new ServiciuValidari();
+		servicii.add(serviciuValidare);
+		servicii.add(serviciuMk);
+		
+		
+	}
+
+	public static void main(String[] args) {
+	
+		Produs produs=new Produs(new StrategieMarketing2021(), new ServiciuValidari());
+		try {
+		float pretFinal=produs.getPretFinal(ro.ase.csie.cts.as.laborator3.faza1.TipProdus.DISCOUNT,100,12);
+	System.out.println("Pretul final este: "+pretFinal);
+	
+	
+	produs.setStrategieMarketing(new InterfataMarketing() {
+		
+		@Override
+		public float getDiscountFidelitate(int vechimeClientInAni) {
+			
+			return 0.3f;
+		}
+	});
+	pretFinal=produs.getPretFinal(ro.ase.csie.cts.as.laborator3.faza1.TipProdus.DISCOUNT, 100, 12);
+	System.out.println("Pretul final este " + pretFinal);
+		}
+		catch (ExceptiePretInvalid e) {
+			
+		}catch(ExceptieVechimeClient e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
