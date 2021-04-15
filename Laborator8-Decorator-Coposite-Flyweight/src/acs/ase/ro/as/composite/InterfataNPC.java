0deff3afec9be5b164b43ae634e628f34ccb5336
OblojaOana <oblojaoana98@gmail.com>
2021-04-15 13:36:29 +0300

    composite

---
+++ Laborator8-Decorator-Coposite-Flyweight/src/acs/ase/ro/as/composite/InterfataNPC.java
@@ -0,0 +1,8 @@
+package acs.ase.ro.as.composite;
+
+public interface InterfataNPC {
+
+	public void seDeplaseaza(int x, int y);
+	public void atacaJucator(int puncte);
+	public void seRetrage();
+}
---
+++ Laborator8-Decorator-Coposite-Flyweight/src/acs/ase/ro/as/composite/UnitateAbstractaJoc.java
@@ -0,0 +1,23 @@
+package acs.ase.ro.as.composite;
+
+
+public abstract class UnitateAbstractaJoc implements InterfataNPC{
+
+	//interfata nodurilor frunza este mostenita
+	//definim interfata specifica nodurilor copil
+	
+	public void adaugaCaracter(UnitateAbstractaJoc unitate)
+	{
+		throw new UnsupportedOperationException();
+	}
+	public void stergeCaracter(UnitateAbstractaJoc unitate)
+	{
+		throw new UnsupportedOperationException();
+	}
+	public UnitateAbstractaJoc getCaracter(int index)
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	
+}
